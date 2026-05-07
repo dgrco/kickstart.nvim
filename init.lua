27565e8d72deb6ab4920e946bb967ad356c85b98
Dante Grieco <dantegrieco11@gmail.com>
2026-05-06 20:54:03 -0700

    change doc show delay

--- init.lua
+++ init.lua
@@ -749,7 +749,7 @@ do
     completion = {
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = true, auto_show_delay_ms = 0 },
+      documentation = { auto_show = true, auto_show_delay_ms = 50 },
       menu = {
         min_width = 15,
         max_height = 10,
